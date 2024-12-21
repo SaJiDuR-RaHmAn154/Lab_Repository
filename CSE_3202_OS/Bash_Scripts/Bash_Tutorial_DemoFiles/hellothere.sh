41e8b8366bde8f572ef86331eeea8a1db2949dff
Sajid <sajid@Sajids-MacBook-Air.local>
2024-12-21 22:51:43 +0600

    Operating System Lab folder uploaded

---
+++ CSE_3202_OS/Bash_Scripts/Bash_Tutorial_DemoFiles/hellothere.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+F_Name=Sajidur
+L_Name=Rahman
+
+echo Hello $F_Name $L_Name
---
+++ CSE_3202_OS/Bash_Scripts/Bash_Tutorial_DemoFiles/interactiveShell.sh
@@ -0,0 +1,8 @@
+  #!/bin/zsh
+
+  echo What is your first name?
+  read F_Name
+  echo What is your last name?
+  read L_Name
+
+  echo Hello $F_Name $L_Name
