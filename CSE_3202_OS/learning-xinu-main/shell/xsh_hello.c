7c8106a8b1663f5e93506ab7ceb517104e502fbf
“SaJiDuR-RaHmAn154” <sajidurrahmantarafder@gmail.com>
2025-04-18 03:59:39 +0600

    Added xinu_os

--- CSE_3202_OS/learning-xinu-main/shell/xsh_hello.c
+++
@@ -1,8 +0,0 @@
-#include <xinu.h>
-#include <stdio.h>
-
-
-shellcmd xsh_hello(int nargs, char *args[])
-{
-    printf("Hello Sajid\n");
-}
--- CSE_3202_OS/learning-xinu-main/shell/xsh_hello2.c
+++
@@ -1,7 +0,0 @@
-#include <xinu.h>
-#include <stdio.h>
-
-shellcmd xsh_greet(int nargs, char *args[])
-{
-    printf("Hello XINU\n");
-}
